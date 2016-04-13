f158a573b5c274d13a69e8a1c72b5f4517215c05
Maximilian Ertl <ertl.maxi@gmail.com>
2016-04-14 00:24:31 +0200

    fixed a typo

--- AutoRemote_eg.py
+++ AutoRemote_eg.py
@@ -778,7 +778,7 @@ class GoogleDrive(object):
                    "Content-Type": contentType,
                    "User-Agent": ("Mozilla/5.0 (Windows NT 6.3; WOW64) "
                                   "AppleWebKit/537.36 (KHTML, like Gecko) "
-                                  "Chrome/30.0.1599.101 Safari/537.36"}
+                                  "Chrome/30.0.1599.101 Safari/537.36")}
         request = urllib2.Request(url, data, headers=headers)
         try:
             f = urllib2.urlopen(request)
